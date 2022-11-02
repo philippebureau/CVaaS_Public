e284af38fe0cdf195116fa0fb3abfb9c997fc8b9
philippebureau <philippe.bureau2@gmail.com>
2022-11-02 11:06:46 -0400

    Create testCvpAccess.py

---
+++ Python/testCvpAccess.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+###### README ######
+# THIS SCRIPT REQUIRED CVPRAC MODULE
+# DOWNLOAD LOCATION: https://github.com/aristanetworks/cvprac
+
+from cvprac.cvp_client import CvpClient
+import urllib.request
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+import requests.packages.urllib3
+
+#disable SSL warnings
+requests.packages.urllib3.disable_warnings()
+
+####
+#USER INPUT
+#INSERT YOUR SERVICE ACCOUNT TOKEN BELOW
+token = '<token>'
+#SET YOUR CVP HOSTNAME OR IP
+CVP = 'CVP'
+####
+
+clnt = CvpClient()
+#clnt.connect(nodes=[CVP], username='', password='', is_cvaas=True, cvaas_token=token)
+clnt.connect(nodes=[CVP], username='', password='', api_token=token)
+result = clnt.api.get_cvp_info()
+print (result)
